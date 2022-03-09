6c9a2e197bbd6374f466f2fbb3541e634edd888c
jim <jim.zhu@ubtrobot.com>
2022-03-09 23:27:40 +0800

    添加string c_str()的测试代码

---
+++ samples/grammer/string/string_c_str_test.cc
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void memcpy_test()
+{
+    string test = "adsadasds";
+    char* buffer = new char[128];
+    memcpy(buffer, test.c_str(), test.size());
+    cout << "buffer: " << buffer << endl;
+    delete []buffer;
+}
+
+int main()
+{
+    memcpy_test();
+}
